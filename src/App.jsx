3eebbded34d82e59c37e68bd61659f3cb3f8b77b
KevinB00 <kevbueeva@alu.edu.gva.es>
2024-04-27 00:09:34 +0200

    Header y Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import CommunityPage from './pages/CommunityPage';
 
 function App() {
   return (
-    <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="community" element={<CommunityPage />} />
         </Routes>
       </BrowserRouter>
-    </>
   );
 }
 
