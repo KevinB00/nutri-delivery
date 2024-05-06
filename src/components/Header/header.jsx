8d815cbf4b275b4715c0de40bf74fcfa3f65bd04
KevinB00 <kevbueeva@alu.edu.gva.es>
2024-05-06 23:08:13 +0200

    rutas arregladas y pedido plato terminado

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,15 +4,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import logo from '../../assets/logo.png';
 import "./header.sass"
 const HeaderComponent = () => {
 
     return (
         <Navbar className="navbar header" bg="primary" expand="lg" >
             <Container fluid>
-                <Navbar.Brand href="home">
+                <Navbar.Brand href="/home">
                     <img
-                        src="./src/assets/logo.png"
+                        src={logo}
                         width="100"
                         height="70"
                         className="d-inline-block align-top logo"
@@ -22,11 +23,11 @@ const HeaderComponent = () => {
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll" className="justify-content-end">
                     <Nav className="me-auto">
-                        <Nav.Link href="home">Principal</Nav.Link>
-                        <Nav.Link href="community">Comunidad</Nav.Link>
+                        <Nav.Link href="/home">Principal</Nav.Link>
+                        <Nav.Link href="/community">Comunidad</Nav.Link>
                         <NavDropdown title="Listas" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="restaurantes">Restaurantes</NavDropdown.Item>
-                            <NavDropdown.Item href="platos">Platos</NavDropdown.Item>
+                            <NavDropdown.Item href="/restaurantes">Restaurantes</NavDropdown.Item>
+                            <NavDropdown.Item href="/platos">Platos</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Button className="btn-login" variant="tertiary" href="/login">Iniciar sesión</Button>
