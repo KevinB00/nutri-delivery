c367db16b0664fc72e3c8cf675b44bf074226748
KevinB00 <kevbueeva@alu.edu.gva.es>
2024-05-19 21:16:35 +0200

    Obtener información de plato

--- src/pages/InfoPlato/infoPlato.jsx
+++ src/pages/InfoPlato/infoPlato.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import FooterComponent from "../../components/Footer/footer";
 import HeaderComponent from "../../components/Header/header";
 import Container from "react-bootstrap/Container";
@@ -7,9 +8,12 @@ import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
 import "./infoPlato.sass";
 
 const InfoPlato = () => {
+  const { platoId } = useParams();
+  const [plato, setPlato] = useState(null);
   const [showForm, setShowForm] = useState(false);
   const [address, setAddress] = useState("");
   const [phone, setPhone] = useState("");
@@ -17,6 +21,20 @@ const InfoPlato = () => {
   const [phoneError, setPhoneError] = useState(false);
   const [addressError, setAddressError] = useState(false);
 
+  useEffect(() => {
+    const fetchPlato = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost/nutri-delivery/backend/actions/read/getInfoPlato.php?id_plato=${platoId}`
+        );
+        const data = await response.json();
+        setPlato(data);
+      } catch (error) {
+        console.error("Error al obtener la información del plato:", error);
+      }
+    };
+    fetchPlato();
+  }, [platoId]);
   const validarDireccion = async (direccion) => {
     try {
       const respuesta = await fetch(
@@ -50,7 +68,6 @@ const InfoPlato = () => {
     return regexNumeroTelefono.test(numeroTelefono);
   };
 
-
   const handleAddressChange = async (event) => {
     const direccion = event.target.value;
     setAddress(direccion);
@@ -76,117 +93,133 @@ const InfoPlato = () => {
     <>
       <HeaderComponent />
       <Container className="mt-5 plato-info">
-        {/* Preview Image */}
-        <img
-          src="plato-preview-url"
-          alt="Plato preview"
-          className="plato-preview"
-        />
+        {plato ? (
+          <>
+            {/* Preview Image */}
+            <Image
+              src={`http://localhost/nutri-delivery/public/${plato.imagen_url}`}
+              alt={plato.nombre}
+              width={500}
+              className="plato-preview"
+              fluid
+              rounded
+            />
 
-        {/* Title */}
-        <h1 className="plato-title">Nombre del plato</h1>
+            {/* Title */}
+            <h1 className="plato-title">{plato.nombre}</h1>
 
-        {/* Description */}
-        <p className="plato-description">Descripción del plato</p>
+            {/* Description */}
+            <p className="plato-description">{plato.descripcion}</p>
 
-        {/* Nutritional Information */}
-        <Container className="plato-nutrition-info">
-          <Row>
-            <Col xs={6} md={3}>
-              <ListGroup variant="flush">
-                <ListGroup.Item>Calorías: 123</ListGroup.Item>
-                <ListGroup.Item>Proteínas: 23g</ListGroup.Item>
-                <ListGroup.Item>Carbohidratos: 34g</ListGroup.Item>
-                <ListGroup.Item>Grasas: 12g</ListGroup.Item>
-              </ListGroup>
-            </Col>
-          </Row>
-        </Container>
+            {/* Nutritional Information */}
+            <Container className="plato-nutrition-info">
+              <Row>
+                <Col xs={6} md={3}>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item>Calorías: {plato.calorias}</ListGroup.Item>
+                    <ListGroup.Item>
+                      Proteínas: {plato.proteinas}g
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      Carbohidratos: {plato.carbohidratos}g
+                    </ListGroup.Item>
+                    <ListGroup.Item>Grasas: {plato.grasas}g</ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Container>
 
-        {/* Price */}
-        <p className="plato-price">
-          Precio: <strong>10€</strong>
-        </p>
+            {/* Price */}
+            <p className="plato-price">
+              Precio: <strong>{plato.precio}€</strong>
+            </p>
 
-        {/* Button to order */}
-        <Container>
-          <Row>
-            <Col className="text-center">
-              <Button
-                variant="primary"
-                className="plato-order-button"
-                onClick={handleShowForm}
-              >
-                Pedir el plato
-              </Button>
-            </Col>
-          </Row>
-        </Container>
+            {/* Button to order */}
+            <Container>
+              <Row>
+                <Col className="text-center">
+                  <Button
+                    variant="primary"
+                    className="plato-order-button"
+                    onClick={handleShowForm}
+                  >
+                    Pedir el plato
+                  </Button>
+                </Col>
+              </Row>
+            </Container>
 
-        {showForm && (
-          <Form onSubmit={handleSubmit} className="mt-3 mb-5">
-            <Form.Group className="mb-3" controlId="formAddress">
-              <Form.Label>Dirección:</Form.Label>
-              <Form.Control
-                type="text"
-                name="address"
-                placeholder="Dirección"
-                required
-                value={address}
-                className={`text ${addressError ? "is-invalid" : ""}`}
-                onChange={handleAddressChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formPhone">
-              <Form.Label>Teléfono:</Form.Label>
-              <Form.Control
-                type="tel"
-                name="phone"
-                placeholder="Teléfono"
-                className={`text ${phoneError ? "is-invalid" : ""}`}
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                onBlur={() => {
-                  if (!validarNumeroTelefono(phone)) {
-                    setPhoneError('El número de teléfono no es válido');
-                  } else {
-                    setPhoneError('');
-                  }
-                }}
-              />
-              <Form.Control.Feedback type="invalid">
-                {phoneError && "Por favor, introduzca un número de teléfono válido (España: 9 dígitos, comenzando por 6, 7, 8 o 9)."}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formCreditCard">
-              <Form.Label>Número de tarjeta de crédito:</Form.Label>
-              <Form.Control
-                type="text"
-                name="creditCard"
-                placeholder="Número de tarjeta de crédito"
-                required
-              />
-            </Form.Group>
-            <Form.Group className="mb-3 w-50 quantityInfo" controlId="formQuantity">
-              <Form.Label>Cantidad:</Form.Label>
-              <Form.Control
-                type="number"
-                name="quantity"
-                placeholder="Cantidad"
-                required
-                min={1}
-                step={1}
-                value={quantity}
-                onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
-              />
-            </Form.Group>
-            <Button variant="tertiary" type="submit">
-              Enviar
-            </Button>
-          </Form>
+            {showForm && (
+              <Form onSubmit={handleSubmit} className="mt-3 mb-5">
+                <Form.Group className="mb-3" controlId="formAddress">
+                  <Form.Label>Dirección:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="address"
+                    placeholder="Dirección"
+                    required
+                    value={address}
+                    className={`text ${addressError ? "is-invalid" : ""}`}
+                    onChange={handleAddressChange}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPhone">
+                  <Form.Label>Teléfono:</Form.Label>
+                  <Form.Control
+                    type="tel"
+                    name="phone"
+                    placeholder="Teléfono"
+                    className={`text ${phoneError ? "is-invalid" : ""}`}
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    onBlur={() => {
+                      if (!validarNumeroTelefono(phone)) {
+                        setPhoneError("El número de teléfono no es válido");
+                      } else {
+                        setPhoneError("");
+                      }
+                    }}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {phoneError &&
+                      "Por favor, introduzca un número de teléfono válido (España: 9 dígitos, comenzando por 6, 7, 8 o 9)."}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formCreditCard">
+                  <Form.Label>Número de tarjeta de crédito:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="creditCard"
+                    placeholder="Número de tarjeta de crédito"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3 w-50 quantityInfo"
+                  controlId="formQuantity"
+                >
+                  <Form.Label>Cantidad:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    name="quantity"
+                    placeholder="Cantidad"
+                    required
+                    min={1}
+                    step={1}
+                    value={quantity}
+                    onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
+                  />
+                </Form.Group>
+                <Button variant="tertiary" type="submit">
+                  Enviar
+                </Button>
+              </Form>
+            )}
+          </>
+        ) : (
+          <p>Cargando...</p>
         )}
       </Container>
-
       <FooterComponent />
     </>
   );
